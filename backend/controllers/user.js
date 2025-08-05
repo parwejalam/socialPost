9ec61d354de556300f618d98a4be3e22b325ce0a
Parwej Alam <parwejalamgt@gmail.com>
2025-08-05 15:04:08 +0530

    feat: update authentication to use environment variables for JWT key and MongoDB password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -57,7 +57,7 @@ exports.loginUser = (req, res, next) => {
             }
             const token = jwt.sign(
                 { email: fetchedUser.email, userId: fetchedUser._id },
-                'secrate_should_be_this_longer',
+                process.env.JWT_KEY,
                 { expiresIn: "1h" });
             res.status(200).json({
                 token: token,
